e6240faf2b82c53f262586cb70d0a7b2c0c9bbe6
godelivadiva <godeliva.divayu@students.utdi.ac.id>
2023-07-14 11:57:37 +0700

    trial

--- assets/js/cat_list.js
+++ assets/js/cat_list.js
@@ -1,63 +1,24 @@
-let db;
-
-// Membuka koneksi IndexedDB
-const request = indexedDB.open('catDB', 1);
-
-// Membuat schema IndexedDB
-request.onupgradeneeded = (event) => {
-  db = event.target.result;
-
-  // Membuat objek store
-  const store = db.createObjectStore('cats', { keyPath: 'id' });
-  store.createIndex('nameIndex', 'name');
-};
-
-request.onerror = (event) => {
-  console.error('Koneksi ke IndexedDB gagal:', event.target.errorCode);
-};
-
-request.onsuccess = (event) => {
-  db = event.target.result;
-
-  // Menyimpan data dari API ke IndexedDB
-  fetch('https://63afb929649c73f572c113ad.mockapi.io/api/v1/cat_adoption_list')
-    .then((response) => response.json())
-    .then((data) => {
-      const transaction = db.transaction('cats', 'readwrite');
-      const store = transaction.objectStore('cats');
-
-      data.forEach((cat) => {
-        store.put(cat);
-      });
-    })
-    .catch((error) => {
-      console.error('Gagal memuat data:', error);
-    });
-
-  // Mengambil data dari IndexedDB
-  getCatsFromIndexedDB()
-    .then((cats) => {
-      // Gunakan data kucing dari IndexedDB
-      console.log(cats);
-    })
-    .catch((error) => {
-      console.error('Gagal mendapatkan data kucing dari IndexedDB:', error);
-    });
-};
-
-// Mengambil data dari IndexedDB
-function getCatsFromIndexedDB() {
-  return new Promise((resolve, reject) => {
-    const transaction = db.transaction('cats', 'readonly');
-    const store = transaction.objectStore('cats');
-    const request = store.getAll();
-
-    request.onsuccess = (event) => {
-      resolve(event.target.result);
-    };
-
-    request.onerror = (event) => {
-      reject(event.target.error);
-    };
+function showCats(cats) {
+  const adoptSection = document.getElementById('adopt');
+  let html = '';
+  cats.forEach((cat) => {
+    html += `
+      <div class="cat-card">
+        <img src="${cat.image_url}" alt="${cat.name}">
+        <h3>${cat.name}</h3>
+        <p>${cat.description}</p>
+      </div>
+    `;
   });
+  adoptSection.innerHTML = html;
 }
+
+// Mengambil data dari REST API
+fetch('https://63afb929649c73f572c113ad.mockapi.io/api/v1/cat_adoption_list')
+  .then((response) => response.json())
+  .then((data) => {
+    showCats(data);
+  })
+  .catch((error) => {
+    console.error('Gagal memuat data:', error);
+  });
---
+++ service-worker.js
@@ -0,0 +1,61 @@
+// Nama cache
+const CACHE_NAME = 'pet-care-cache-v1';
+
+// Daftar file yang akan di-cache
+const urlsToCache = [
+  '/',
+  'index.html',
+  'assets/css/style.css',
+  'assets/js/script.js',
+];
+
+// Install Service Worker dan caching file
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Menghapus cache yang tidak diperlukan saat Service Worker diaktivasi
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.filter((cacheName) => {
+            return cacheName !== CACHE_NAME;
+          }).map((cacheName) => {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+  );
+});
+
+// Mengintersep permintaan fetch dan mencoba mencari data dari cache terlebih dahulu
+// Jika tidak ditemukan di cache, permintaan akan dilakukan ke server dan data akan dicache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});

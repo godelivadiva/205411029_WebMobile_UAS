e46f61fad2b4b165ca02ea6502c0cfdf8daf1dd2
godelivadiva <godeliva.divayu@students.utdi.ac.id>
2023-07-14 20:42:40 +0700

    test

--- assets/js/cat_list.js
+++ assets/js/cat_list.js
@@ -1,22 +1,30 @@
-function showCats(cats) {
-  const adoptSection = document.getElementById('adopt');
-  cats.forEach((cat) => {
-    const catCard = `
-            <div class="col-md-4">
-              <div class="card mb-4">
-                <img src="${cat.image_url}" class="card-img-top" alt="${cat.name}">
-                <div class="card-body">
-                  <h5 class="card-title">${cat.name}</h5>
-                  <p class="card-text">${cat.description}</p>
-                  <p class="card-text">Gender: ${cat.gender}</p>
-                  <p class="card-text">Age: ${cat.age} years old</p>
-                </div>
-              </div>
+// Menggunakan data dari IndexedDB
+getCatsFromIndexedDB()
+  .then((cats) => {
+    // Gunakan data kucing dari IndexedDB
+    const catListElement = document.getElementById('catList');
+
+    cats.forEach((cat) => {
+      const catCard = `
+        <div class="col-md-4">
+          <div class="card mb-4">
+            <img src="${cat.image_url}" class="card-img-top" alt="${cat.name}">
+            <div class="card-body">
+              <h5 class="card-title">${cat.name}</h5>
+              <p class="card-text">${cat.description}</p>
+              <p class="card-text">Gender: ${cat.gender}</p>
+              <p class="card-text">Age: ${cat.age} years old</p>
             </div>
-          `;
-    adoptSection.innerHTML += catCard;
+          </div>
+        </div>
+      `;
+      catListElement.innerHTML += catCard;
+    });
+  })
+  .catch((error) => {
+    console.error('Gagal mendapatkan data kucing dari IndexedDB:', error);
   });
-}
+
 
 // Mengambil data dari REST API
 fetch('https://63afb929649c73f572c113ad.mockapi.io/api/v1/cat_adoption_list')

be84b4ddb44ba6e8fb30debbfeb01a336f0ec22c
godelivadiva <godeliva.divayu@students.utdi.ac.id>
2023-07-16 15:23:18 +0700

    comment serviceworker js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-// Nama cache
+// nama cache
 const CACHE_NAME = 'pet-care-cache-v1';
 
-// Daftar file yang akan di-cache
+// file yang akan di-cache beserta dengan URLnya karena tanpa URL, akan muncul error 
+// Failed to execute 'addAll' on 'Cache'
 const urlsToCache = [
   'https://godelivadiva.github.io/205411029_WebMobile_UAS/',
   'https://godelivadiva.github.io/205411029_WebMobile_UAS/index.html',
@@ -10,16 +11,24 @@ const urlsToCache = [
 ];
 
 // Install Service Worker dan caching file
+// caching file: proses menyimpan file sementara
+// service worker harus menunggu hingga 
+// dimana perintah open akan membuka atau membuat cache
+// method addAll untuk menambahkan file yang akan di-cache ke dalam cache yang telah dibuka sebelumnya 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 // Menghapus cache yang tidak diperlukan saat Service Worker diaktivasi
+// service worker akan menghapus semua cache yang tidak sesuai dengan CACHE_NAME, 
+// sehingga hanya cache yang digunakan untuk tetap ada di service worker
+// bertujuan untuk membersihkan cache yang sudah tidak digunakan lagi dan 
+// memastikan service worker selalu menggunakan cache yang diperbarui dan relevan.
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys()
@@ -35,8 +44,10 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Mengintersep permintaan fetch dan mencoba mencari data dari cache terlebih dahulu
-// Jika tidak ditemukan di cache, permintaan akan dilakukan ke server dan data akan dicache
+// Jika ada response yang sesuai di cache, maka service worker akan mengembalikan response tersebut 
+// sebagai respon untuk fetch.
+// Jika tidak ada response yang sesuai di cache, maka service worker akan melakukan fetch ke server 
+// untuk mendapatkan response baru, lalu menyimpannya ke dalam cache untuk digunakan pada fetch selanjutnya.
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
